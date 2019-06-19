eccb12181e6c60e2cecacdf822d1713bed2449ec
calmgust <calmgust@gmail.com>
2019-06-19 13:58:31 +0900

    add sequelize setting and modify app

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+//TODO: add router
 
-var app = express();
+const sequelize = require('./models').sequelize;
+
+const app = express();
+sequelize.sync();
+// sequelize.sync({force: true});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
