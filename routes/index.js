40005e05093cf77092bd0203f1a0cdf72966b36c
calmgust <calmgust@gmail.com>
2019-06-20 13:24:59 +0900

    change dist

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
+var sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  // const sgMail = require('@sendgrid/mail');
-  // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  // const msg = {
-  //   to: 'kimms@gananetworks.com',
-  //   from: 'kimms@gananetworks.com',
-  //   subject: 'Sending with Twilio SendGrid is Fun',
-  //   text: 'and easy to do anywhere, even with Node.js',
-  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  // };
-  // sgMail
-  //   .send(msg)
-  //   .then(function (msg) { console.log('success : ', msg) })
-  //   .catch(function (err) { console.log('fail : ', err) });
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  console.log('2')
+  var msg = {
+    to: 'kimms@gananetworks.com',
+    from: 'kimms@gananetworks.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail
+    .send(msg)
+    .then(function (msg) { console.log('success : ', msg) })
+    .catch(function (err) { console.log('fail : ', err) });
   
 });
 
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 /* GET post listening. */
-router.get('/sendmail', function(req, res, next) {
-  
-  const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
+router.get('/sendmail', function (req, res, next) {
+  console.log('1')
+  var msg = {
     to: 'kimms@gananetworks.com',
     from: 'kimms@gananetworks.com',
     subject: 'Sending with Twilio SendGrid is Fun',
@@ -15,8 +16,8 @@ router.get('/sendmail', function(req, res, next) {
   };
   sgMail
     .send(msg)
-    .then(function (msg) { console.log('success : ', msg) })
-    .catch(function (err) { console.log('fail : ', err)});
+    .then(msg => console.log('success : ', msg))
+    .catch(err => console.log('fail : ', err));
 
 });
 
