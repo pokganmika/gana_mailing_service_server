688e42ced63a386825d659c66c05396ba320ee10
pokganmika <calmgust@gmail.com>
2019-07-11 11:11:53 +0900

    create scheduled mail routes (pause / cancel / retrieve)

--- routes/mailEdit.js
+++ routes/mailEdit.js
@@ -8,11 +8,6 @@ const api_key = process.env.SENDGRID_API_KEY;
 const sgClient = require('@sendgrid/client');
 sgClient.setApiKey(api_key);
 
-const testUnixTime = new Date().getTime();
-const changeTime = new Date(testUnixTime);
-console.log('::testUnixTime:: ---> : ', testUnixTime)
-console.log('::change time:: ---> : ', changeTime);
-
 router.get('/', async (req, res, next) => {
   const nowUnixTime = new Date().getTime();
   const oldData = [];
@@ -48,5 +43,64 @@ router.get('/', async (req, res, next) => {
  * delete
  */
 
+router.post('/pause', async (req, res, next) => { 
+  const { batch_id } = req.body;
+  const request = {
+    body: {
+      batch_id,
+      status: 'pause'
+    },
+    method: 'POST',
+    url: '/v3/user/scheduled_sends'
+  };
+  await sgClient.request(request)
+    .then(([response, body]) => {
+      console.log(body);
+      res.send('::later::mail::edit::success::pause::')
+    })
+    .catch(err => {
+      console.log(err);
+      res.send('::later::mail::edit::fail::pause::');
+    })
+})
+
+router.post('/cancel', async (req, res, next) => { 
+  const { batch_id } = req.body;
+  const request = {
+    qs: {
+      batch_id,
+      status: 'cancel'
+    },
+    method: 'POST',
+    url: '/v3/user/scheduled_sends'
+  };
+  await sgClient.request(request)
+    .then(([response, body]) => {
+      console.log(body);
+      res.send('::later::mail::edit::success::cancel::')
+    })
+    .catch(err => {
+      console.log(err);
+      res.send('::later::mail::edit::fail::cancel::');
+    })
+})
+
+router.get('/retrieve/:batchId', async (req, res, next) => { 
+  const { batchId } = req.params;
+
+  const request = {
+    method: 'GET',
+    url: `/v3/user/scheduled_sends/${batchId}`
+  };
+  await sgClient.request(request)
+    .then(([response, body]) => {
+      console.log(body);
+      res.send('::later::mail::edit::success::retrive::');
+    })
+    .catch(err => { 
+      console.log(err);
+      res.send('::later::mail::edit::fail::retrive::');
+    })
+})
 
 module.exports = router;
